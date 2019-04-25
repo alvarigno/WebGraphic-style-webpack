4b89ed855c657f17ebebc58aaf3cf805e6ecfb18
alvaro.emparan@iconstruye.onmicrosoft.com <alvaro.emparan@iconstruye.onmicrosoft.com>
2019-04-25 16:47:15 -0400

    update

--- WebPackProject/src/app.js
+++ WebPackProject/src/app.js
@@ -2,3 +2,6 @@ import './scss/app.scss';
 import './scss/javascripts/bootstrap/carousel'
 import './scss/javascripts/bootstrap/dropdown'
 import './scss/javascripts/bootstrap/tab'
+import './scss/javascripts/bootstrap/transition'
+import './scss/javascripts/bootstrap/tooltip'
+import './scss/javascripts/bootstrap/scrollspy'
